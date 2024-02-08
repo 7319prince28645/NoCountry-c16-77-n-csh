d6ebedb90314a4b8aa21285ba8ccaeca38322ddc
Prince Moreno <prins_ghost_2018@outlook.com>
2024-02-08 18:31:07 -0500

    First

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Experiencias from './pages/Experiencias'
+import Navbar from './pages/Navbar'
+import Populares from './pages/Populares'
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <Populares/>
+      <Experiencias/>
+    </>
+  )
+}
+
+export default App
+
---
+++ src/pages/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+        <h1>PRince</h1>
+        <div>
+            <a href="/cursos">Cursos</a>
+            <a href="/experiencias">Experiencias</a>
+            <a href="/populares">Populares</a>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
