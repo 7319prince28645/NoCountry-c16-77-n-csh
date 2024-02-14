ee98be6ea7510105c24b3bdad333f8d41e923b4e
Prince Moreno <prins_ghost_2018@outlook.com>
2024-02-14 12:53:18 -0500

    Ya

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://github.com/7319prince28645/NoCountry-c16-77-n-csh",
 })
