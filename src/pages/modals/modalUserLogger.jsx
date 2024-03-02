676907f7ccdd0829c5e1d8698d414f442d5b1219
Enrique Ginfeu <ginfeu.enrique@hotmail.com>
2024-03-02 11:57:49 -0300

    panel user

--- src/pages/modals/modalUserLogger.jsx
+++ src/pages/modals/modalUserLogger.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { useState } from "react";
+import '../sctruct/css/users.css'
+import user from '../../assets/user-logo.svg'
+import compras from '../../assets/compras-logo.svg'
+import cerrar from '../../assets/cerrar-logo-7.svg'
+import MiCuenta from "../sctruct/MiCuenta";
+import MisCompras from "../sctruct/MisCompras";
+
+
 function ModalUserLogger() {
   const [clic, setClic] = useState("");
   const handleClick = (link) => {
@@ -11,25 +19,60 @@ function ModalUserLogger() {
     localStorage.removeItem("usuario");
     window.location.href = "/";
   };
-  return (
-    <div className="w-52">
-      <h2
-        className="text-2xl font-bold cursor-pointer"
-        onClick={() => navigate("/")}
-      >
-        User Accounts
-      </h2>
+  const [selectedItemIndex, setSelectedItemIndex] = useState(0);
 
-      <Link
-        to={"/accounts/dashboard/home"}
-        className={
-          clic === "home" ? "hover:text-gray-300" : "hover:text-gray-300"
-        }
-        onClick={handleCerrarSesion}
-      >
-        Cerrar sesión
-      </Link>
-    </div>
+  function handleLiClick(index) {
+    setSelectedItemIndex(index);
+  }
+  return (
+    <>
+      <div className="grid grid-cols-1 md:grid-cols-[1fr_4fr] bg-gray-100 gap-2 xl:px-20">
+        <div className="px-2">
+          <ul className="space-y-5">
+            <li className={`bg-li py-3 pl-5 rounded-md ${selectedItemIndex === 0 ? 'active' : ''}`}>
+              <a href="#" className="flex gap-4" onClick={() => handleLiClick(0) }>
+                <span><img src={user} alt="" /></span>Mi cuenta
+              </a>
+            </li>
+            <li className={`bg-li py-3 pl-5 rounded-md ${selectedItemIndex === 1 ? 'active' : ''}`}>
+              <a href="#" className="flex gap-4" onClick={() => handleLiClick(1)}>
+                <span><img src={compras} alt="" /></span>Mis compras
+              </a>
+            </li>
+            <li className="bg-li py-3 pl-5">
+            <Link
+          to={"/accounts/dashboard/home"}
+          className="flex gap-4"
+          onClick={handleCerrarSesion}
+        >
+          <span><img src={cerrar} alt="" /></span>Cerrar sesión
+        </Link>
+            </li>
+          </ul>
+        </div>
+
+        {selectedItemIndex === 0 && <MiCuenta />}
+        {selectedItemIndex === 1 && <MisCompras />}
+        
+
+      </div>
+
+      <div className="w-52">
+        <h2 className="text-2xl font-bold cursor-pointer">
+          User Accounts
+        </h2>
+
+        <Link
+          to={"/accounts/dashboard/home"}
+          className={
+            clic === "home" ? "hover:text-gray-300" : "hover:text-gray-300"
+          }
+          onClick={handleCerrarSesion}
+        >
+          Cerrar sesión
+        </Link>
+      </div>
+    </>
   );
 }
 
---
+++ src/pages/sctruct/MiCuenta.jsx
@@ -0,0 +1,155 @@
+
+
+function Micuenta() {
+  return (
+    <>
+        <div className="mt-10 rounded-lg xl:mt-0">
+          <h2 className="pl-5 xl:pl-0 xl:text-xl font-semibold border-b-[1px]">Mi cuenta</h2>
+
+          <div className="shadow-xl rounded-lg">
+            <h2 className="pt-5 pl-5 border-b-[1px] xl:text-lg">
+              Datos de contacto
+            </h2>
+            <form className='grid xl:grid-cols-3 gap-5 mt-10 p-4'>
+              <div>
+                <label htmlFor="name" className="block text-sm text-gray-500">
+                  Nombre
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Maria"
+                  className='py-1 px-1 w-full'
+                />
+              </div>
+              <div>
+                <label htmlFor="name" className="block text-sm text-gray-500">
+                  Apellido
+                </label>
+                <input
+                  type="text"
+                  name="apellido"
+                  placeholder="Maria"
+                  className='py-1 px-1 w-full'
+                />
+              </div>
+              <div>
+                <label htmlFor="name" className="block text-sm text-gray-500">
+                  Nro. Documento
+                </label>
+                <input
+                  type="text"
+                  name="documentNumber"
+                  placeholder="30500600"
+                  className='py-1 px-1 w-full'
+                />
+              </div>
+              <div>
+                <label htmlFor="name" className="block text-sm text-gray-500">
+                  Email de Contacto
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="marioyjuli@hola.com"
+                  className='py-1 px-1 w-full'
+                />
+              </div>
+              <div>
+                <label htmlFor="name" className="block text-sm text-gray-500">
+                  Cod. Area
+                </label>
+                <input
+                  type="tel"
+                  name="codeNumber"
+                  placeholder="011"
+                  className='py-1 px-1 w-full'
+                />
+              </div>
+              <div>
+                <label htmlFor="name" className="block text-sm text-gray-500">
+                  Telefono
+                </label>
+                <input
+                  type="tel"
+                  name="phoneNumber"
+                  placeholder="2525-2525"
+                  className='py-1 px-1 w-full'
+                />
+              </div>
+            </form>
+            <div className="flex justify-end p-3">
+              <button
+                type="submit"
+                className="bg-[#FFA62b] text-white px-4 py-1 rounded hover:bg-[#ffaf3e] xl:w-1/6"
+              >
+                Guardar Cambios
+              </button>
+            </div>
+          </div>
+          <div className="grid xl:grid-cols-2 mt-1 gap-3">
+            <div className="shadow-2xl rounded-lg">
+              <h2 className="pt-5 pl-5 border-b-[1px] text-lg">
+                Mi acceso
+              </h2>
+              <div className="gap-5 p-4 space-y-5 mt-4">
+                <div>
+                  <h2>Email</h2>
+                  <h3>marioyjuli@hola.com</h3>
+                </div>
+                <div>
+                  <h2>Acceso con Google</h2>
+                  <h3>No</h3>
+                </div>
+                <div>
+                  <h2>Acceso con Facebook</h2>
+                  <h3>No</h3>
+                </div>
+              </div>
+            </div>
+            <div className="shadow-2xl rounded-lg">
+              <h2 className="pt-5 pl-5 border-b-[1px] text-lg">
+                Cambiar Contraseña
+              </h2>
+              <form action="" className="gap-5 p-4 space-y-5">
+                <div>
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Contraseña Actual"
+                    className='py-1 px-1 w-full'
+                  />
+                </div>
+                <div>
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Nueva Contraseña"
+                    className='py-1 px-1 w-full'
+                  />
+                </div>
+                <div>
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Repetir Contraseña"
+                    className='py-1 px-1 w-full'
+                  />
+                </div>
+              </form>
+              <div className="flex justify-end p-3">
+              <button
+                type="submit"
+                className="bg-[#FFA62b] text-white px-4 py-1 rounded hover:bg-[#ffaf3e] xl:w-1/3"
+              >
+                Guardar Cambios
+              </button>
+            </div>
+            </div>
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default Micuenta
